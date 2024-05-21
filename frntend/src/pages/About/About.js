9e5a0a70a1b58432f02ce536a10f947af2d50180
Prerna Tanwar <98750278+PT-02@users.noreply.github.com>
2024-05-22 00:26:46 +0530

    updates

--- frntend/src/pages/About/About.js
+++ frntend/src/pages/About/About.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Footer from '../../components/Footer/Footer';
+import './aboutpage.css'
 
 export default function About() {
   return (
-    <div className='About'>
-        <div>
+    <div className='main'>
+        <div className='about'>
             <img src="foods/aboutus.png" alt="" />
-                  </div>
+        </div>
+      <Footer/>
     </div>
   )
 }
